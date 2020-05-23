03f7b9eca53ece87376894c5bdf5ae52f59bf338
dnegometyanov <denis.engometyanov@gmail.com>
2020-05-23 08:08:58 +0300

    Disabled search on data tables

--- assets/js/app.js
+++ assets/js/app.js
@@ -46,6 +46,7 @@ $(document).ready(function () {
 
                         $(".datatable").DataTable({
                             data: quotes,
+                            "searching": false,
                             columns: [
                                 {title: "Date", data: "dateString"},
                                 {title: "Open", data: "open"},
